a8d7159c805ac150c44e1b63f8b5f4c3e3c2ceb2
Josh de la Peña <josh.delapena@stationfive.com>
2020-07-22 14:33:55 +0800

    add i18n Internationalization

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 
+import { ThemeProvider } from 'emotion-theming';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from 'routes';
+import theme from 'utils/Theme';
+
+// Initialize languages
+import './locales/i18n';
 
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+const App = () => (
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Router>
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
+    </Router>
+  </React.StrictMode>
 );
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/locales/i18n.ts
@@ -0,0 +1,55 @@
+import i18next from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+import { ConvertedToObjectType } from 'types/Locale';
+
+import en from './en/translation.json';
+
+// Add json translations here
+const translationsJson = {
+  en: {
+    translation: en,
+  },
+};
+
+export type TranslationResource = typeof en;
+export type LanguageKey = keyof TranslationResource;
+
+export const translations: ConvertedToObjectType<TranslationResource> = {} as any;
+
+/*
+ * Converts the static JSON file into an object where keys are identical
+ * but values are strings concatenated according to syntax.
+ * This is helpful when using the JSON file keys and still have the intellisense support
+ * along with type-safety
+ */
+const convertLanguageJsonToObject = (obj: any, dict: any, current?: string) => {
+  Object.keys(obj).forEach((key) => {
+    const currentLookupKey = current ? `${current}.${key}` : key;
+    if (typeof obj[key] === 'object') {
+      dict[key] = {};
+      convertLanguageJsonToObject(obj[key], dict[key], currentLookupKey);
+    } else {
+      dict[key] = currentLookupKey;
+    }
+  });
+};
+
+export const i18n = i18next
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init(
+    {
+      resources: translationsJson,
+      lng: 'en',
+      debug: process.env.NODE_ENV !== 'production',
+    },
+    () => {
+      convertLanguageJsonToObject(en, translations);
+    }
+  );
