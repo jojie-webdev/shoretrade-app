7f8f176478e88449259e3f2a2b16ce6f3e903f2d
Jerameel <jerameel.dr@stationfive.com>
2020-07-30 09:31:44 +0800

    Update themes

--- src/types/Theme.ts
+++ src/types/Theme.ts
@@ -1,3 +1,36 @@
 export type Theme = {
   //  Define themes here
+  appType: 'buyer' | 'seller';
+  brand: {
+    primary: string;
+    secondary: string;
+    info: string;
+    success: string;
+    warning: string;
+    error: string;
+  };
+  grey: {
+    shade1: string;
+    shade2: string;
+    shade3: string;
+    shade5: string;
+    shade6: string;
+    shade7: string;
+    shade8: string;
+    shade9: string;
+    noshade: string;
+  };
+  fonts: {
+    BasisGrotesquePro: {
+      Light: string;
+      Regular: string;
+      Medium: string;
+      Bold: string;
+      Black: string;
+      LightItalic: string;
+      RegularItalic: string;
+      BoldItalic: string;
+      BlackItalic: string;
+    };
+  };
 };
--- src/utils/Theme.ts
+++ src/utils/Theme.ts
@@ -3,6 +3,62 @@ import { Theme } from 'types/Theme';
 
 export const useTheme = (): Theme => useThemeBase();
 
-export const theme: Theme = {};
+const basisfontWeightMapping: { [s: string]: number } = {
+  Light: 300,
+  Regular: 400,
+  Medium: 500,
+  Bold: 700,
+  Black: 900,
+};
+
+const generateFontStyles = (
+  fontName: string,
+  fontWeightMapping: { [s: string]: number }
+) => {
+  return Object.keys(fontWeightMapping).reduce(
+    (acc, fontWeight) => ({
+      ...acc,
+      [fontWeight]: `
+          font-family: ${fontName};
+          font-weight: ${fontWeightMapping[fontWeight]};
+        `,
+      [`${fontWeight}Italic`]: `
+          font-family: ${fontName};
+          font-weight: ${fontWeightMapping[fontWeight]};
+          font-style: italic;
+        `,
+    }),
+    {}
+  );
+};
+
+export const theme: Theme = {
+  appType: 'seller', // should be overriden on provider level
+  brand: {
+    primary: '#E35D32',
+    secondary: '#B6B4CA',
+    info: '#1877F2',
+    success: '#00C48C',
+    warning: '#FFCF5C',
+    error: '#F23742',
+  },
+  grey: {
+    shade1: '#F9FAFF',
+    shade2: '#EDEFFA',
+    shade3: '#E5E9F5',
+    shade5: '#BBC2DC',
+    shade6: '#7F8498',
+    shade7: '#565A6A',
+    shade8: '#111E2B',
+    shade9: '#09131D',
+    noshade: '#FFFFFF',
+  },
+  fonts: {
+    BasisGrotesquePro: generateFontStyles(
+      'Basis Grotesque Pro',
+      basisfontWeightMapping
+    ) as Theme['fonts']['BasisGrotesquePro'],
+  },
+};
 
 export default theme;
