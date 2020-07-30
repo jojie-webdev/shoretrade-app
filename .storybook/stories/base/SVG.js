59995f7032f36bb28e1c6dcaa3aff8d99aef8911
Jerameel <jerameel.dr@stationfive.com>
2020-07-30 15:17:03 +0800

    Add plop for svg entry

---
+++ .storybook/stories/base/SVG.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import {
+  /* PLOP_INJECT_IMPORT */
+  Eye,
+  EyeOff,
+} from '../../../src/components/base/SVG';
+import Container from '../../components/Container';
+
+storiesOf('base/SVG', module).add('Summary', () => {
+  const [text, setText] = useState('');
+
+  // eslint-disable-next-line react/prop-types
+  const Content = ({ children }) => {
+    return (
+      <div
+        style={{ display: 'flex', flexDirection: 'row', flexWrap: 'nowrap' }}
+      >
+        {children}
+      </div>
+    );
+  };
+
+  // eslint-disable-next-line react/prop-types
+  const Wrapper = ({ children, label }) => {
+    return (
+      <div style={{ marginLeft: 16, marginTop: 16 }}>
+        {children}
+        <div>{label}</div>
+      </div>
+    );
+  };
+
+  return (
+    <Container>
+      <Content>
+        {/* PLOP_INJECT_INSTANCE*/}
+        <Wrapper label="Eye">
+          <Eye width={30} height={30} />
+        </Wrapper>
+        <Wrapper label="EyeOff">
+          <EyeOff width={30} height={30} />
+        </Wrapper>
+      </Content>
+    </Container>
+  );
+});
---
+++ src/components/base/SVG/index.tsx
@@ -0,0 +1,3 @@
+/* PLOP_INJECT_IMPORT */
+export { default as Eye } from './Eye';
+export { default as EyeOff } from './EyeOff';
