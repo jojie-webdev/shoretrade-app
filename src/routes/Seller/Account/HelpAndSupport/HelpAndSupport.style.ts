8a8bd05f7ebcd072c124b64cb7c8fe562c1adbf1
Sean Urgel <seantheurgel@gmail.com>
2020-08-10 13:47:01 +0800

    Finish Help And Support

--- src/routes/Seller/Account/HelpAndSupport/HelpAndSupport.style.ts
+++ src/routes/Seller/Account/HelpAndSupport/HelpAndSupport.style.ts
@@ -1,3 +1,22 @@
+import Accordion from 'components/base/Accordion';
 import styled from 'utils/styled';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  .help-text {
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 24px;
+    margin: 0;
+    margin-bottom: 24px;
+
+    span {
+      color: ${(props) => props.theme.brand.primary};
+    }
+  }
+
+  .accordion-container {
+    margin-bottom: 8px;
+  }
+`;
+
+export const StyledAccordion = styled(Accordion)``;
