3bec5f3cd56761e62ebef477296c446bf09d3df7
Sean Urgel <seantheurgel@gmail.com>
2020-08-12 11:12:32 +0800

    Add Boxes

--- src/routes/Seller/AddProduct/AddProduct.style.ts
+++ src/routes/Seller/AddProduct/AddProduct.style.ts
@@ -169,4 +169,44 @@ export const Step6Wrapper = styled.div`
   }
 `;
 
-export const BoxDetailsContainer = styled.div``;
+export const BoxDetailsContainer = styled.div`
+  background: ${(props) => props.theme.grey.shade9};
+  border-radius: 4px;
+  padding: 16px 24px;
+  margin-bottom: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .text-container {
+    display: flex;
+
+    .inner-text {
+      margin-right: 40px;
+
+      .overline {
+        margin-bottom: 2px;
+      }
+    }
+  }
+
+  .cancel-btn {
+    border-radius: 100%;
+    border: none;
+    height: 24px;
+    width: 24px;
+    background: none;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export const BoxSummaryContainer = styled.div`
+  padding: 25px 80px;
+
+  .text-container {
+    .inner-text {
+      margin-right: 40px;
+    }
+  }
+`;
--- src/routes/Seller/AddProduct/Step6.tsx
+++ src/routes/Seller/AddProduct/Step6.tsx
@@ -2,18 +2,65 @@ import React, { useState } from 'react';
 
 import Button from 'components/base/Button';
 import Checkbox from 'components/base/Checkbox';
-import { Box } from 'components/base/SVG';
+import { Box, Subtract } from 'components/base/SVG';
 import TextField from 'components/base/TextField';
 import Typography from 'components/base/Typography';
 import Add from 'components/module/Add';
 import AddBoxModal from 'components/module/AddBoxModal';
+import { BoxValues } from 'components/module/AddBoxModal/AddBoxModal.props';
 import { Row, Col } from 'react-grid-system';
 
-import { Step6Wrapper } from './AddProduct.style';
+import { Step6Wrapper, BoxDetailsContainer } from './AddProduct.style';
 
-const BoxDetails = () => (
-    
-)
+const MOCK_BOXES: Array<BoxValues & { id: string }> = [
+  { weight: '10.00 Kg', quantity: '7', count: '56', id: 'mock-box-1' },
+  { weight: '25.00 Kg', quantity: '10', count: '25', id: 'mock-box-2' },
+  { weight: '5.00 Kg', quantity: '3', count: '27', id: 'mock-box-3' },
+];
+
+const BoxDetails = ({ weight, quantity, count }: BoxValues) => (
+  <BoxDetailsContainer>
+    <div className="text-container">
+      <div className="inner-text">
+        <Typography
+          variant="overline"
+          color="shade6"
+          className="overline"
+          weight="900"
+        >
+          Box Weight
+        </Typography>
+        <Typography color="noshade">{weight}</Typography>
+      </div>
+      <div className="inner-text">
+        <Typography
+          variant="overline"
+          color="shade6"
+          className="overline"
+          weight="900"
+        >
+          Quantity
+        </Typography>
+        <Typography color="noshade">{quantity}</Typography>
+      </div>
+      <div className="inner-text">
+        <Typography
+          variant="overline"
+          color="shade6"
+          className="overline"
+          weight="900"
+        >
+          Count per Box
+        </Typography>
+        <Typography color="noshade">{count}</Typography>
+      </div>
+    </div>
+
+    <button className="cancel-btn">
+      <Subtract />
+    </button>
+  </BoxDetailsContainer>
+);
 
 function Step6() {
   const [showModal, setShowModal] = useState(false);
@@ -36,7 +83,13 @@ function Step6() {
       </Row>
 
       <Row className="add-box-row">
-        <Col>
+        {MOCK_BOXES.map((box) => (
+          <Col md={12} key={box.id}>
+            <BoxDetails {...box} />
+          </Col>
+        ))}
+
+        <Col md={12}>
           <Add title="Add a box" Svg={Box} onClick={() => setShowModal(true)} />
         </Col>
       </Row>
