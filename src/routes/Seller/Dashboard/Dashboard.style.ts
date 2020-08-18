41f6303900800d02a70178ebb20cd9fa9300de07
Sean Urgel <seantheurgel@gmail.com>
2020-08-18 08:37:11 +0800

    Added new sm and unselected button. Added dashboard filter buttons

--- src/routes/Seller/Dashboard/Dashboard.style.ts
+++ src/routes/Seller/Dashboard/Dashboard.style.ts
@@ -1,3 +1,15 @@
+import { Row } from 'react-grid-system';
 import styled from 'utils/styled';
 
 export const Container = styled.div``;
+
+export const FilterRow = styled(Row)`
+  .filter-col {
+    display: flex;
+    align-items: center;
+
+    .btn {
+      margin-right: 12px;
+    }
+  }
+`;
--- src/routes/Seller/Dashboard/Dashboard.view.tsx
+++ src/routes/Seller/Dashboard/Dashboard.view.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
 
 // import { useTheme } from 'utils/Theme';
+import Button from 'components/base/Button';
+import { Row, Col } from 'react-grid-system';
+
 import { DashboardGeneratedProps } from './Dashboard.props';
-import { Container } from './Dashboard.style';
+import { Container, FilterRow } from './Dashboard.style';
 
 const DashboardView = (props: DashboardGeneratedProps) => {
   // const theme = useTheme();
   return (
     <Container>
+      <FilterRow>
+        <Col className="filter-col">
+          <Button
+            text="Custom"
+            size="sm"
+            variant="unselected"
+            className="btn"
+          />
+          <Button
+            text="FY19/20"
+            size="sm"
+            variant="unselected"
+            className="btn"
+          />
+          <Button text="Q4" size="sm" variant="unselected" className="btn" />
+          <Button text="Q3" size="sm" variant="unselected" className="btn" />
+          <Button text="Q2" size="sm" variant="unselected" className="btn" />
+          <Button text="Q1" size="sm" variant="unselected" className="btn" />
+          <Button
+            text="FY18/19"
+            size="sm"
+            variant="unselected"
+            className="btn"
+          />
+          <Button
+            text="FY17/18"
+            size="sm"
+            variant="unselected"
+            className="btn"
+          />
+        </Col>
+      </FilterRow>
       <h1>Dashboard Screen</h1>
     </Container>
   );
