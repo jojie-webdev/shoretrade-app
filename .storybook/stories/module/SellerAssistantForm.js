4a6fe64fb1e66c6daed618a347c11150c1ee4229
Sean Urgel <seantheurgel@gmail.com>
2020-09-01 14:16:22 +0800

    added storybook

--- .storybook/stories/module/SellerAssistantForm.js
+++ .storybook/stories/module/SellerAssistantForm.js
@@ -5,8 +5,27 @@ import { storiesOf } from '@storybook/react';
 import SellerAssistantForm from '../../../src/components/module/SellerAssistantForm';
 import Container from '../../components/Container';
 
+const formikInitial: FormikForm = {
+  initialValues: {
+    firstName: '',
+    lastName: '',
+    email: '',
+    mobile: '',
+  },
+  onSubmit: () => {},
+};
+
+const props = {
+  type: 'EDIT',
+  formikInitial,
+  callingCode: '61',
+  pending: false,
+  loading: false,
+  onClickDelete: () => {},
+};
+
 storiesOf('module/SellerAssistantForm', module).add('Summary', () => (
   <Container>
-    <SellerAssistantForm />
+    <SellerAssistantForm {...props} />
   </Container>
 ));
