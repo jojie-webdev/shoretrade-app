035819fdd23bf5fa855b7b25bf1c641ae96e7b6b
jamesukun <james.saballegue@stationfive.com>
2020-09-25 09:23:21 +0800

    Added buyer help page

---
+++ src/routes/Buyer/Account/HelpAndSupport/HelpAndSupport.container.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import HelpAndSupportView from './HelpAndSupport.view';
+
+const HelpAndSupport = (): JSX.Element => {
+  const generatedProps = {
+    // generated props here
+  };
+  return <HelpAndSupportView {...generatedProps} />;
+};
+
+export default HelpAndSupport;
