7165d87477a3264d663b27adab51b9eb70d3964d
Christian Umali <christian.umali@stationfive.com>
2020-09-28 15:11:19 +0800

    Fix responsiveness of preview cards according to screensize

--- src/routes/Buyer/Categories/Preview/Preview.style.ts
+++ src/routes/Buyer/Categories/Preview/Preview.style.ts
@@ -23,16 +23,46 @@ export const PreviewContainer = styled.div`
   height: 100%;
   overflow: scroll;
   overflow-x: hidden;
-
+  display: table;
+  clear: both;
   ${customScrollbar}
 
   .cards {
     display: flex;
     flex-wrap: wrap;
+    clear: both;
   }
 
   .search-row {
     margin-bottom: 24px;
+  }
+
+  /* Float four columns side by side */
+  .column {
+    float: left;
+    /* width: 30%; */
+    padding: 0 10px;
+  }
+
+  /* Remove extra left and right margins, due to padding */
+  .row {
+    margin: 0 -5px;
+  }
+
+  /* Clear floats after the columns */
+  .row:after {
+    content: '';
+    display: table;
+    clear: both;
+  }
+
+  /* Responsive columns */
+  @media screen and (max-width: 600px) {
+    .column {
+      width: 100%;
+      display: block;
+      margin-bottom: 20px;
+    }
   }
 `;
 
