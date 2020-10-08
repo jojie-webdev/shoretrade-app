fc5d5caffdcb8c4bba5d463bfc784ef670b156f7
jamesukun <james.saballegue@stationfive.com>
2020-10-08 15:21:14 +0800

    Added Credit History

---
+++ src/routes/Buyer/Account/BalanceHistory/BalanceHistory.view.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import Typography from 'components/base/Typography';
+import InnerRouteHeader from 'components/module/InnerRouteHeader';
+import Loading from 'components/module/Loading';
+import moment from 'moment';
+import { Col, Row } from 'react-grid-system';
+import { toPrice } from 'utils/String/toPrice';
+
+// import { useTheme } from 'utils/Theme';
+import { BalanceHistoryGeneratedProps } from './BalanceHistory.props';
+import { Container, Transx, TransxLeft, TransxRight } from './BalanceHistory.style';
+
+const BalanceHistoryView = (props: BalanceHistoryGeneratedProps) => {
+  // const theme = useTheme();}
+
+  return (
+    <Container>
+      <InnerRouteHeader title="Credit History" />
+      <Col md={12}>
+        {props.isLoading && <Loading />}
+        {props.transactions.map((transaction, idx) => {
+          return (
+            <Row key={idx}>
+              <Transx>
+                <TransxLeft>
+                  <Typography variant="body" color="shade9">
+                    {transaction.description}
+                  </Typography>
+                  <Typography variant="caption" color="shade6">
+                    {moment(transaction.createdAt).format('DD MMM YYYY')}
+                  </Typography>
+                </TransxLeft>
+                <TransxRight>
+                  <Typography variant="body" color="shade9">
+                    {toPrice(transaction.adjustmentAmount)}
+                  </Typography>
+                  <Typography variant="caption" color="shade6">
+                    Balance: {toPrice(transaction.balance)}
+                  </Typography>
+                </TransxRight>
+              </Transx>
+            </Row>
+          );
+        })}
+      </Col>
+    </Container>
+  );
+};
+
+export default BalanceHistoryView;
---
+++ src/store/sagas/getTransactionHistory.ts
@@ -0,0 +1,38 @@
+import { put, call, takeLatest, select } from 'redux-saga/effects';
+import {
+  GetTransactionHistoryMeta,
+  GetTransactionHistoryPayload,
+} from 'types/store/GetTransactionHistoryState';
+import { AsyncAction } from 'types/Action';
+import { Store } from 'types/store/Store';
+import { getTransactionHistory } from 'services/company';
+import { getTransactionHistoryActions } from '../actions';
+
+function* getTransactionHistoryRequest(
+  action: AsyncAction<GetTransactionHistoryMeta, GetTransactionHistoryPayload>,
+) {
+  const state: Store = yield select();
+  if (state.auth.token) {
+    try {
+      const { data } = yield call(
+        getTransactionHistory,
+        action.meta,
+        state.auth.token,
+      );
+      yield put(getTransactionHistoryActions.success(data));
+    } catch (e) {
+      yield put(getTransactionHistoryActions.failed(e.message));
+    }
+  } else {
+    yield put(getTransactionHistoryActions.failed('Token not found'));
+  }
+}
+
+function* getTransactionHistoryWatcher() {
+  yield takeLatest(
+    getTransactionHistoryActions.REQUEST,
+    getTransactionHistoryRequest,
+  );
+}
+
+export default getTransactionHistoryWatcher;
