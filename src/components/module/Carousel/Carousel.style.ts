139dd2e92b3c88740e7b7912c032787941da3015
Sean Urgel <sean.urgel@stationfive.com>
2020-10-08 05:47:00 +0000

    Merged in qa/listing-details (pull request #166)
    fix image aspect ratio and fix listing details
    
    * fix image aspect ratio and fix listing details
    
    Approved-by: Jerameel Delos Reyes

--- src/components/module/Carousel/Carousel.style.ts
+++ src/components/module/Carousel/Carousel.style.ts
@@ -25,6 +25,7 @@ export const ImageContainer = styled.div`
 export const Image = styled.img`
   width: 100%;
   height: 100%;
-  object-fit: cover;
+  object-fit: contain;
+  background: ${(props) => props.theme.grey.shade6};
   border-radius: 4px;
 `;
