fcf5226298b36592c660719772c779c789292b3a
SeanningTatum <seantheurgel@gmail.com>
2020-10-15 11:11:42 +0800

    Added examples on storybook

--- .storybook/stories/module/SellerCard.js
+++ .storybook/stories/module/SellerCard.js
@@ -3,10 +3,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import SellerCard from '../../../src/components/module/SellerCard';
+import mockImage from '../../../src/res/images/seller-profile-default.png';
 import Container from '../../components/Container';
 
 storiesOf('module/SellerCard', module).add('Summary', () => (
-  <Container>
-    <SellerCard />
+  <Container style={{ width: '250px' }}>
+    <SellerCard companyImage={mockImage} companyName="Mock Company Name" />
   </Container>
 ));
