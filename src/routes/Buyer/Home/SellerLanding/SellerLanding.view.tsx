3beeff0da169f998f49155f2b91a9e1cfc3cba34
SeanningTatum <seantheurgel@gmail.com>
2020-10-17 09:12:04 +0800

    fix seller view responsiveness

--- src/routes/Buyer/Home/SellerLanding/SellerLanding.view.tsx
+++ src/routes/Buyer/Home/SellerLanding/SellerLanding.view.tsx
@@ -2,16 +2,12 @@ import React, { useEffect } from 'react';
 
 // import { useTheme } from 'utils/Theme';
 import Search from 'components/module/Search';
+import SellerCard from 'components/module/SellerCard';
 import { Row, Col } from 'react-grid-system';
 import { Link } from 'react-router-dom';
 
 import { SellerLandingGeneratedProps } from './SellerLanding.props';
-import {
-  PreviewContainer,
-  CardContainer,
-  SellerCardTypography,
-  SellerContainer,
-} from './SellerLanding.style';
+import { PreviewContainer, SellerContainer } from './SellerLanding.style';
 
 const SellerLanding = (props: SellerLandingGeneratedProps) => {
   // const theme = useTheme();
@@ -31,28 +27,19 @@ const SellerLanding = (props: SellerLandingGeneratedProps) => {
       </Row>
       {results.length > 0 ? (
         <>
-          <Row className="cards" style={{ marginTop: 20 }}>
-            <SellerContainer>
+          <SellerContainer>
+            <Row style={{ marginTop: 20 }}>
               {results.map((s) => {
                 return (
                   <Col xxl={2} xl={3} md={4} sm={6} key={s.id}>
                     <Link to={`/buyer/seller-details/${s.id}`}>
-                      <CardContainer className="centered">
-                        <div className="card">
-                          <img src={s.companyImage} alt={s.companyImage} />
-                          <div className="card-content">
-                            <SellerCardTypography>
-                              {s.companyName}
-                            </SellerCardTypography>
-                          </div>
-                        </div>
-                      </CardContainer>
+                      <SellerCard {...s} />
                     </Link>
                   </Col>
                 );
               })}
-            </SellerContainer>
-          </Row>
+            </Row>
+          </SellerContainer>
         </>
       ) : null}
     </PreviewContainer>
