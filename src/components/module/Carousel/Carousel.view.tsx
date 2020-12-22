a9d5d759677a10f4a2f2f4087a7b61de38a2faca
SeanningTatum <seantheurgel@gmail.com>
2020-12-22 08:24:27 +0800

    fixes

--- src/components/module/Carousel/Carousel.view.tsx
+++ src/components/module/Carousel/Carousel.view.tsx
@@ -150,7 +150,7 @@ const Carousel = (props: CarouselProps): JSX.Element => {
           loop={loop && images.length > 1}
           initialSlide={0}
           autoplay={
-            images.length !== 0 && autoplay
+            false && images.length !== 0 && autoplay
               ? {
                   delay: 5000,
                 }
