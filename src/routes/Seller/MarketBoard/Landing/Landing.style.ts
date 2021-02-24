f3c4fb858564e99de8b84b439926641adacae02e
Josh de la Peña <josh.delapena@stationfive.com>
2021-02-24 18:31:07 +0800

    add interactionns

--- src/routes/Seller/MarketBoard/Landing/Landing.style.ts
+++ src/routes/Seller/MarketBoard/Landing/Landing.style.ts
@@ -1,3 +1,4 @@
+import { BREAKPOINTS } from 'consts/breakpoints';
 import styled from 'utils/styled';
 
 export const Container = styled.div`
@@ -7,5 +8,41 @@ export const Container = styled.div`
 
   .interactions {
     margin-bottom: 16px;
+
+    .left-component {
+      display: flex;
+      align-items: center;
+
+      img {
+        width: 92px;
+        height: 92px;
+        border-radius: 8px;
+        margin-right: 16px;
+      }
+
+      .badges-container {
+        display: flex;
+        flex-wrap: wrap;
+        width: 350px;
+        margin: 4px 0;
+
+        @media ${BREAKPOINTS['sm']} {
+          width: 100%;
+        }
+
+        .badge {
+          margin: 0 4px 4px 0;
+        }
+      }
+
+      .weights {
+        display: flex;
+        align-items: center;
+
+        svg {
+          margin: 0 6px;
+        }
+      }
+    }
   }
 `;
--- src/routes/Seller/MarketBoard/Landing/Landing.view.tsx
+++ src/routes/Seller/MarketBoard/Landing/Landing.view.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 
-// import { useTheme } from 'utils/Theme';
+import Badge from 'components/base/Badge/Badge.view';
 import Interactions from 'components/base/Interactions';
 import SegmentedControls from 'components/base/SegmentedControls/SegmentedControls.view';
+import { ArrowRight } from 'components/base/SVG';
+import Typography from 'components/base/Typography';
+import { BadgeText } from 'components/module/CategoryCards/Preview/Preview.style';
 import Search from 'components/module/Search';
 import { Col, Row } from 'react-grid-system';
+import { useTheme } from 'utils/Theme';
 
 import { MarketBoardLandingGeneratedProps, TabOptions } from './Landing.props';
 import { Container } from './Landing.style';
 
 const MarketBoardLandingView = (props: MarketBoardLandingGeneratedProps) => {
-  // const theme = useTheme();
+  const theme = useTheme();
+
   return (
     <Container>
       <SegmentedControls
@@ -36,9 +41,91 @@ const MarketBoardLandingView = (props: MarketBoardLandingGeneratedProps) => {
         </Col>
       </Row>
 
-      {[...new Array(10)].map((v, i) => (
-        <Interactions key={i} padding="16px 24px 16px 16px" />
-      ))}
+      {props.currentTab === 'Buyer Requests' &&
+        [...new Array(5)].map((v, i) => (
+          <Interactions
+            key={i}
+            leftComponent={
+              <div className="left-component">
+                <img src="https://picsum.photos/200/300" />
+                <div>
+                  <Typography color="noshade">Pale Octopus</Typography>
+                  <div className="badges-container">
+                    {['Fresh', 'Farmed', 'Head on Gutted', 'Medium'].map(
+                      (v) => (
+                        <Badge
+                          key={v}
+                          className="badge"
+                          badgeColor={theme.grey.shade8}
+                        >
+                          <BadgeText variant="overlineSmall" color="noshade">
+                            {v}
+                          </BadgeText>
+                        </Badge>
+                      )
+                    )}
+                  </div>
+
+                  <div className="weights">
+                    <Typography color="noshade" variant="small">
+                      100kg
+                    </Typography>
+                    <ArrowRight
+                      width={10}
+                      height={10}
+                      fill={theme.grey.shade7}
+                    />
+                    <Typography color="noshade" variant="small">
+                      250kg
+                    </Typography>
+                  </div>
+                </div>
+              </div>
+            }
+            padding="16px 24px 16px 16px"
+          />
+        ))}
+
+      {props.currentTab === 'My Active Offers' &&
+        [...new Array(5)].map((v, i) => (
+          <Interactions
+            key={i}
+            leftComponent={
+              <div className="left-component">
+                <img src="https://picsum.photos/200/300" />
+                <div>
+                  <Typography color="noshade">Pale Octopus</Typography>
+                  <div className="badges-container">
+                    <Badge
+                      key={v}
+                      className="badge"
+                      badgeColor={theme.brand.success}
+                    >
+                      <BadgeText variant="overlineSmall" color="noshade">
+                        ACCEPTED
+                      </BadgeText>
+                    </Badge>
+                  </div>
+
+                  <div className="weights">
+                    <Typography color="noshade" variant="small">
+                      100kg
+                    </Typography>
+                    <ArrowRight
+                      width={10}
+                      height={10}
+                      fill={theme.grey.shade7}
+                    />
+                    <Typography color="noshade" variant="small">
+                      250kg
+                    </Typography>
+                  </div>
+                </div>
+              </div>
+            }
+            padding="16px 24px 16px 16px"
+          />
+        ))}
     </Container>
   );
 };
