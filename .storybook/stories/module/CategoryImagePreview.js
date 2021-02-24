f2c1ed014b0b379c3cb15b36e7fb2b23a63ed891
Juno <juno.deguzman@stationfive.com>
2021-02-24 08:37:43 +0000

    fix storybook categoryImagePreview

--- .storybook/stories/module/CategoryImagePreview.js
+++ .storybook/stories/module/CategoryImagePreview.js
@@ -6,7 +6,11 @@ import CategoryImagePreview from '../../../src/components/module/CategoryImagePr
 import Container from '../../components/Container';
 
 storiesOf('module/CategoryImagePreview', module).add('Summary', () => (
-  <Container>
-    <CategoryImagePreview />
+  <Container appType="buyer">
+    <CategoryImagePreview
+      categoryName="Sample Category"
+      caption="Nulla occaecat commodo cillum laborum ullamco amet adipisicing consequat officia magna. Quis in exercitation Lorem minim. "
+      imgSrc="https://picsum.photos/id/995/500/500.jpg"
+    />
   </Container>
 ));
