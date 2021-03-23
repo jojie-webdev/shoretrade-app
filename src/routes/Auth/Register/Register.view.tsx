3b60bb34cece6e61c2ac8b14bd6d824f14b1a801
Josh de la Peña <josh.delapena@stationfive.com>
2021-03-23 11:14:34 +0800

    fix step

--- src/routes/Auth/Register/Register.view.tsx
+++ src/routes/Auth/Register/Register.view.tsx
@@ -1066,7 +1066,7 @@ const RegisterView = (props: RegisterGeneratedProps) => {
     isSummaryEdit,
   } = props;
 
-  const [step, setStep] = useState(2);
+  const [step, setStep] = useState(0);
   const MAX_STEP = !isSeller ? 6 : 7;
 
   const summaryHandleStep = (step: number) => {
