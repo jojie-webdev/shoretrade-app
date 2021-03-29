a5ed1d0c125fde66962de783cf54c3f483a43365
Josh de la Peña <josh.delapena@stationfive.com>
2021-03-29 16:26:45 +0800

    update help and support

--- src/routes/Seller/Account/HelpAndSupport/HelpAndSupport.view.tsx
+++ src/routes/Seller/Account/HelpAndSupport/HelpAndSupport.view.tsx
@@ -18,7 +18,7 @@ const HELP_AND_SUPPORT = [
   },
   {
     title: 'What do I need to sign up?',
-    description: `The signing process is simple, all you need is your business name,
+    description: `The signing up process is simple, all you need is your business name,
     ABN, banking details and address. Our team will review your
     application before you can start listing and selling your products in
     our seafood marketplace.`,
@@ -30,7 +30,7 @@ const HELP_AND_SUPPORT = [
   },
   {
     title: 'Can I sell overseas?',
-    description: `International exports is launching early 2019 for fishermen and
+    description: `International exports is launching early 2021 for fishermen and
     fisheries with valid export licences.`,
   },
   {
