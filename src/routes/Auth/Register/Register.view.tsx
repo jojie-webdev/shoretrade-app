f7c606f09f8842aa057ceacf30b68ccc3e30d51b
Don Cosio <don.cosio@stationfive.com>
2021-04-05 13:16:28 +0800

    added ...

--- src/routes/Auth/Register/Register.view.tsx
+++ src/routes/Auth/Register/Register.view.tsx
@@ -255,7 +255,7 @@ const StepForm = ({
           <Search height={16} width={16} />
           <input
             type="search"
-            placeholder="Search for a product"
+            placeholder="Search for a product..."
             onChange={(e) => onChangeSearch(e.target.value)}
             value={searchTerm}
           />
