dd1e52958ee33a706aa590d99b4f18bc1daef8be
Josh de la Peña <josh.delapena@stationfive.com>
2021-05-05 16:54:00 +0800

    fix segmented controls

---
+++ src/routes/Seller/MarketBoard/Landing/Landing.constants.ts
@@ -0,0 +1,4 @@
+export const BuyerRequestsTooltip =
+  'Buyer requests detail products buyers want to purchase. You will be able to view requests and make offers. ' +
+  'Both parties can negotiate the price until an offer is accepted. ' +
+  "It is the seller's responsibility to organise shipping of these products to the buyer.";
