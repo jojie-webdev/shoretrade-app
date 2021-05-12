77badd51f3463ea3c98760c236a3dcc9d21991f9
Josh de la Peña <josh.delapena@stationfive.com>
2021-05-12 10:40:43 +0800

    update breadcrumb and seller assistant form for mobile

--- src/components/base/Breadcrumbs/Breadcrumbs.style.ts
+++ src/components/base/Breadcrumbs/Breadcrumbs.style.ts
@@ -1,8 +1,13 @@
+import { BREAKPOINTS } from 'consts/breakpoints';
 import styled from 'utils/styled';
 
 export const Container = styled.div`
   display: flex;
   align-items: center;
+
+  @media ${BREAKPOINTS['sm']} {
+    flex-wrap: wrap;
+  }
 
   .divider {
     margin: 0 16px;
