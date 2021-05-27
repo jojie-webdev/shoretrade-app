c0c68da409a28274fb0b07dcb728a824554281af
Josh de la Peña <josh.delapena@stationfive.com>
2021-05-27 13:09:19 +0800

    fix padding on accounts

--- src/routes/Seller/Account/HelpAndSupport/HelpAndSupport.style.ts
+++ src/routes/Seller/Account/HelpAndSupport/HelpAndSupport.style.ts
@@ -1,6 +1,11 @@
+import { BREAKPOINTS } from 'consts/breakpoints';
 import styled from 'utils/styled';
 
 export const Container = styled.div`
+  @media ${BREAKPOINTS['sm']} {
+    padding-bottom: 24px;
+  }
+
   .breadcrumb-container {
     margin-bottom: 40px;
   }
