c6bddc2740eea37eb76cb564d7ed55075132dbff
Don Cosio <don.cosio@stationfive.com>
2021-06-18 13:20:14 +0800

    clean up

--- src/routes/Buyer/Categories/Preview/Preview.props.ts
+++ src/routes/Buyer/Categories/Preview/Preview.props.ts
@@ -25,11 +25,3 @@ export interface CategoriesPreviewGeneratedProps {
   //   showUngraded?: boolean;
   // }) => void;
 }
-
-export type FilterData = {
-  catchmentArea?: any;
-  sizeRangeFrom?: number | string;
-  sizeRangeTo?: number | string;
-  specifications?: any;
-  showUngraded?: boolean;
-};
