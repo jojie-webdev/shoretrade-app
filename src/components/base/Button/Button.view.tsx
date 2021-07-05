d720ec503594dc5d635c9cc4f804effdd6fda19c
Juno <juno.deguzman@stationfive.com>
2021-07-05 15:05:53 +0800

    cleanup

--- src/components/base/Button/Button.view.tsx
+++ src/components/base/Button/Button.view.tsx
@@ -60,8 +60,6 @@ const Button = (props: ButtonProps): JSX.Element => {
 
   const hasText = !!(text && text?.length > 0);
 
-  console.log(color);
-
   return (
     <ButtonContainer
       size={size}
