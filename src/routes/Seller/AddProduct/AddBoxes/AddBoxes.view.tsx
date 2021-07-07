485eb7da0949217ea985c926d5de8201a6d3c273
Josh de la Peña <josh.delapena@stationfive.com>
2021-07-07 09:37:58 +0800

    Revert "retain boxes on back/next"
    This reverts commit a49b9e0a6b27c4648aeb2d4ede9c436d38149e19.

--- src/routes/Seller/AddProduct/AddBoxes/AddBoxes.view.tsx
+++ src/routes/Seller/AddProduct/AddBoxes/AddBoxes.view.tsx
@@ -12,10 +12,8 @@ import { BoxValues } from 'components/module/AddBoxModal/AddBoxModal.props';
 import { BREAKPOINTS } from 'consts/breakpoints';
 import remove from 'ramda/es/remove';
 import { Row, Col } from 'react-grid-system';
-import { useDispatch } from 'react-redux';
 import { useMediaQuery } from 'react-responsive';
 import { Aquafuture } from 'routes/Seller/AddProduct/AddPackaging/AddPackaging.style';
-import { editableListingActions } from 'store/actions';
 import { GetCategoryData } from 'store/selectors/seller/categories';
 import { formatMeasurementUnit } from 'utils/Listing/formatMeasurementUnit';
 import theme, { useTheme } from 'utils/Theme';
@@ -222,7 +220,6 @@ const AddBoxes = ({
   isExisting,
   navBack,
 }: AddBoxesProps) => {
-  const dispatch = useDispatch();
   const isMobile = useMediaQuery({ query: BREAKPOINTS['sm'] });
 
   const categoryData = GetCategoryData(
@@ -292,14 +289,6 @@ const AddBoxes = ({
       setShowAlert(false);
     }
   }, [boxes, minimumOrder, showAlert]);
-
-  useEffect(() => {
-    dispatch(
-      editableListingActions.update({
-        boxes,
-      })
-    );
-  }, [boxes]);
 
   return (
     <Container>
