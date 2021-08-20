9897dac16d582f8efcc5f4ef2c6acb736aae13fc
aljohn <johnnjill3@stationfive.com>
2021-08-20 11:20:23 +0800

    removed animated components

--- src/components/base/AnimatedCrab/AnimatedCrab.props.ts
+++
@@ -1,5 +0,0 @@
-import { IPlayerProps } from '@lottiefiles/react-lottie-player';
-
-export interface AnimatedCrabProps extends Omit<IPlayerProps, 'src'> {
-  style?: Record<string, string>;
-}
--- src/components/base/AnimatedOctopus/AnimatedOctopus.props.ts
+++
@@ -1,5 +0,0 @@
-import { IPlayerProps } from '@lottiefiles/react-lottie-player';
-
-export interface AnimatedOctopusProps extends Omit<IPlayerProps, 'src'> {
-  style?: Record<string, string>;
-}
