a7b04f1336e124e5213d9742309470a6ece6ab8d
Sam Tagalog <samuel.tagalog@stationfive.com>
2021-08-26 04:33:43 +0000

    Merged in fix/fiscal-year (pull request #1079)
    [For Review] - Update fiscal year logic
    
    * update fiscal year
    
    * update fiscal year implem
    
    * remove comment
    
    
    Approved-by: Jerameel Delos Reyes

--- src/utils/Date/getFiscalYear.ts
+++ src/utils/Date/getFiscalYear.ts
@@ -1,4 +1,4 @@
 import moment from 'moment';
 
 export default () =>
-  moment().get('month') < 8 ? moment().get('year') - 1 : moment().get('year');
+  moment().get('month') >= 6 ? moment().get('year') : moment().get('year') - 1;
