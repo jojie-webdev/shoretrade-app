93478e8fa23827346273b1a910616617aa607dff
Jerameel <jerameel.dr@stationfive.com>
2021-09-21 11:38:38 +0800

    Add socket integration for read and delete notification

--- src/store/sagas/getNotifications.ts
+++ src/store/sagas/getNotifications.ts
@@ -28,29 +28,123 @@ function* getNotifications(
 
 function* getNotificationsPatch(action: Action<any>) {
   const state: Store = yield select();
-  const notifications = state.getNotifications.data;
+  const notificationsData = state.getNotifications.data;
   const newNotification: any | undefined = pathOr(
     undefined,
     ['payload'],
     action
   );
 
-  if (notifications && newNotification) {
-    const notificationsData = pathOr(
+  if (notificationsData && newNotification) {
+    const notifications = pathOr(
       [],
       ['data', 'notifications'],
-      notifications
+      notificationsData
     );
-    const unreadData = pathOr(0, ['data', 'unread'], notifications);
-    const totalData = pathOr(0, ['data', 'total'], notifications);
+    const unreadData = pathOr(0, ['data', 'unread'], notificationsData);
+    const totalData = pathOr(0, ['data', 'total'], notificationsData);
 
     const modifiedNotificationsData: GetNotificationsPayload = {
-      ...notifications,
+      ...notificationsData,
       data: {
-        ...notifications.data,
-        notifications: [newNotification, ...notificationsData],
+        ...notificationsData.data,
+        notifications: [newNotification, ...notifications],
         total: totalData + 1,
         unread: unreadData + 1,
+      },
+    };
+
+    yield put(getNotificationsActions.patch(modifiedNotificationsData));
+  }
+}
+
+function* getNotificationsReadPatch(action: Action<any>) {
+  const state: Store = yield select();
+  const notificationsData = state.getNotifications.data;
+  const targetNotification: any | undefined = pathOr(
+    undefined,
+    ['payload'],
+    action
+  );
+
+  if (notificationsData && targetNotification) {
+    const notifications = pathOr<any[]>(
+      [],
+      ['data', 'notifications'],
+      notificationsData
+    );
+
+    const modifiedNotifications = notifications.map((a) => {
+      if (a.id === targetNotification.id) {
+        return {
+          ...a,
+          read_at: targetNotification.read_at,
+        };
+      }
+      return a;
+    });
+
+    const unreadData = pathOr(0, ['data', 'unread'], notificationsData);
+
+    const modifiedNotificationsData: GetNotificationsPayload = {
+      ...notificationsData,
+      data: {
+        ...notificationsData.data,
+        notifications: modifiedNotifications,
+        // avoid subtracting if the notification is already marked as unread
+        unread:
+          notifications.findIndex(
+            (a) => a.id === targetNotification.id && a.read_at === null
+          ) !== -1
+            ? unreadData - 1
+            : unreadData,
+      },
+    };
+
+    yield put(getNotificationsActions.patch(modifiedNotificationsData));
+  }
+}
+
+function* getNotificationsDeletePatch(action: Action<any>) {
+  const state: Store = yield select();
+  const notificationsData = state.getNotifications.data;
+  const targetNotification: any | undefined = pathOr(
+    undefined,
+    ['payload'],
+    action
+  );
+
+  if (notificationsData && targetNotification) {
+    const notifications = pathOr<any[]>(
+      [],
+      ['data', 'notifications'],
+      notificationsData
+    );
+
+    const modifiedNotifications = notifications.filter(
+      (a) => a.id !== targetNotification.id
+    );
+
+    const unreadData = pathOr(0, ['data', 'unread'], notificationsData);
+    const totalData = pathOr(0, ['data', 'total'], notificationsData);
+
+    const modifiedNotificationsData: GetNotificationsPayload = {
+      ...notificationsData,
+      data: {
+        ...notificationsData.data,
+        notifications: modifiedNotifications,
+        // avoid subtracting if the notification is already deleted
+        total:
+          notifications.findIndex((a) => a.id === targetNotification.id) !== -1
+            ? totalData - 1
+            : totalData,
+        // avoid subtracting if the notification is already marked as unread
+        unread:
+          notifications.findIndex(
+            (a) => a.id === targetNotification.id && a.read_at === null
+          ) !== -1
+            ? unreadData - 1
+            : unreadData,
       },
     };
 
@@ -61,6 +155,14 @@ function* getNotificationsPatch(action: Action<any>) {
 function* getNotificationsSettingsWatcher() {
   yield takeLatest(getNotificationsActions.REQUEST, getNotifications);
   yield takeLatest(socketActions.INAPP_NOTIFICATION, getNotificationsPatch);
+  yield takeLatest(
+    socketActions.INAPP_NOTIFICATION_READ,
+    getNotificationsReadPatch
+  );
+  yield takeLatest(
+    socketActions.INAPP_NOTIFICATION_DELETED,
+    getNotificationsDeletePatch
+  );
 }
 
 export default getNotificationsSettingsWatcher;
--- src/types/store/SocketState.ts
+++ src/types/store/SocketState.ts
@@ -2,7 +2,9 @@ export type SOCKET_EVENT =
   | 'NEW_ORDER'
   | 'NEW_CREDIT'
   | 'UPDATE_REMAINING_BOXES'
-  | 'INAPP_NOTIFICATION';
+  | 'INAPP_NOTIFICATION'
+  | 'INAPP_NOTIFICATION_READ'
+  | 'INAPP_NOTIFICATION_DELETED';
 
 export type SocketState = {
   connected: boolean;
