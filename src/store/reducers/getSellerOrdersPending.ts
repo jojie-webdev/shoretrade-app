82636e27fed4c1ad9b76d02425067c526d031551
Lea Labagala <lea.labagala@stationfive.com>
2021-12-09 10:13:26 +0800

    STM-43 fix bug

--- src/store/reducers/getSellerOrdersPending.ts
+++ src/store/reducers/getSellerOrdersPending.ts
@@ -8,22 +8,22 @@ import { getSellerOrdersPendingActions } from '../actions';
 import { updateToConfirmed } from './getSellerOrdersPlaced';
 
 export default createAsyncReducer<GetSellerOrdersMeta, GetSellerOrdersPayload>(
-  getSellerOrdersPendingActions,
-  (state, action) => {
-    return {
-      [getSellerOrdersPendingActions.UPDATE_OPTIMISTICALLY]: {
-        ...state,
-        data: {
-          ...state.data,
-          data: {
-            ...state.data?.data,
-            orders: updateToConfirmed(
-              state.data?.data?.orders || [],
-              action.meta || {}
-            ),
-          },
-        },
-      } as any,
-    };
-  }
+  getSellerOrdersPendingActions
+  // (state, action) => {
+  //   return {
+  //     [getSellerOrdersPendingActions.UPDATE_OPTIMISTICALLY]: {
+  //       ...state,
+  //       data: {
+  //         ...state.data,
+  //         data: {
+  //           ...state.data?.data,
+  //           orders: updateToConfirmed(
+  //             state.data?.data?.orders || [],
+  //             action.meta || {}
+  //           ),
+  //         },
+  //       },
+  //     } as any,
+  //   };
+  // }
 );
