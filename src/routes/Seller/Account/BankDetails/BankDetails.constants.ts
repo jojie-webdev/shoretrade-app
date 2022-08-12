b134ce1210adf37a20b8011c712f022052b09e9b
Aljohn Villarubia <aljohn.villarubia@stationfive.com>
2022-08-12 12:42:03 +0000

    Merged in SHOR-404-2 (pull request #2462)
    up
    
    * up

--- src/routes/Seller/Account/BankDetails/BankDetails.constants.ts
+++ src/routes/Seller/Account/BankDetails/BankDetails.constants.ts
@@ -1,2 +1,2 @@
 export const BUSINESS_DETAILS_MESSAGE =
-  'To update your bank account details, please email info@sfmblue.com and request to update your bank account, detailing the Account Name, BSB and Account Number.';
+  'To update your bank account details, please email info@sfmblue.com.au and request to update your bank account, detailing the Account Name, BSB and Account Number.';
--- src/routes/Seller/Account/YourDetails/YourDetails.constants.ts
+++ src/routes/Seller/Account/YourDetails/YourDetails.constants.ts
@@ -1,2 +1,2 @@
 export const BUSINESS_NUMBER_MESSAGE =
-  'To update your business number, please email info@sfmblue.com and detail your Company Name, Full Name and Business Number with a request to change your details';
+  'To update your business number, please email info@sfmblue.com.au and detail your Company Name, Full Name and Business Number with a request to change your details';
