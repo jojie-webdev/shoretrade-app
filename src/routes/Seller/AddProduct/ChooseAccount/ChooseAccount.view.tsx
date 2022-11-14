268cdbb2980562e496690d266c94b007de6e6adf
Nestor Alveyra <nestor.alveyra@stationfive.com>
2022-11-14 00:36:54 +0000

    Fix bulk upload template

--- src/routes/Seller/AddProduct/ChooseAccount/ChooseAccount.view.tsx
+++ src/routes/Seller/AddProduct/ChooseAccount/ChooseAccount.view.tsx
@@ -13,6 +13,7 @@ import { Row, Col } from 'react-grid-system';
 import { useMediaQuery } from 'react-responsive';
 //@ts-ignore
 import preAuctionBulkUploadTemplate from 'res/docs/pre_auction_bulk_upload_template_1668144231.xlsx';
+
 import { AccountOption, ChooseAccountProps } from './ChooseAccount.props';
 import {
   Container,
@@ -196,11 +197,7 @@ const ChooseAccount = ({
             <a
               ref={aRef}
               href={preAuctionBulkUploadTemplate}
-<<<<<<< HEAD
-              download="pre_auction_bulk_upload_template_1668144231.xlsx"
-=======
               download="pre_auction_bulk_upload_template.xlsx"
->>>>>>> dev
               onClick={(e) => e.stopPropagation()}
             >
               Pre-Auction Template
