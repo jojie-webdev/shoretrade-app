b6b66fecc36e6bc954bd062e21145595151740cc
Aljohn Villarubia <aljohn.villarubia@stationfive.com>
2022-11-14 12:34:53 +0000

    Merged in SHOR-840 (pull request #2741)
    [FOR REVIEW] SHOR-840 [Buyer][Web] Scanning History - New depot update
    
    * SHOR-840 [Buyer][Web] Scanning History - New depot update

--- src/consts/markets.ts
+++ src/consts/markets.ts
@@ -3,8 +3,9 @@ import { MarketRoles } from 'types/MarketRoles';
 export const MARKET_BY_ROLES: MarketRoles = {
   DEFAULT: 'Sydney Fish Market',
   SFM_OPS: 'Sydney Fish Market',
-  RAPTIS_OPS: 'Raptis Seafood',
+  RAPTIS_OPS: 'Custom Seafood Distributors',
   MCLAUGHLIN_OPS: 'Melbourne Seafood Centre',
+  SA_OPS: 'SA Seafood Express',
 };
 
 export const MARKET_GROUP_1 = [
