a7280cbc9391ae2cd5ebb57a5f2cd607467161a5
Aljohn Villarubia <aljohn.villarubia@stationfive.com>
2023-02-01 20:06:42 +0800

    Product Details Card 6 > integrate negotiation button

--- src/routes/Buyer/Categories/Preview/Preview.container.tsx
+++ src/routes/Buyer/Categories/Preview/Preview.container.tsx
@@ -237,11 +237,6 @@ const CategoriesPreview = (): JSX.Element => {
   //   );
   // };
 
-  console.log(
-    'defaultCompany?.credit  >>>>>>>>>>>>>> ',
-    defaultCompany?.credit
-  );
-
   const generatedProps = {
     searchValue,
     onChangeSearchValue,
